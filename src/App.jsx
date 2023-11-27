280029c765f7839ca5c36d761bf51fe4e20b664d
Abhijith J <abhijithj47s0@gmail.com>
2023-11-27 15:04:17 +0530

    basic-functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import AddPostForm from "./features/posts/AddPostForm"
+import PostsList from "./features/posts/PostsList"
+
  
  
 function App() {
  
   return (
-    <>
-       App
-    </>
+   <main className="App">
+    <AddPostForm/>
+<PostsList/>
+   </main>
   )
 }
 
--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -1,12 +1,37 @@
 
 
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectAllPosts } from './postSlice'
+import PostAuthor from './PostAuthor'
+import TimeAgo from './TimeAgo'
+import ReactionButton from './ReactionButton'
 
 const PostsList = () => {
+
+    
+    const posts=useSelector(selectAllPosts)
+
+    const orderedPosts=posts.slice().sort((a,b)=>b.date.localeCompare(a.date))
+
+
+    const renderedPosts=orderedPosts.map(post=>(
+<article>
+        <h3>{post.title}</h3>
+        <p>{post.content.substring(0,100)}</p>
+        <p className='postCredit'>
+            <PostAuthor userId={post.userId}/>
+            <TimeAgo timestamp={post.date}/>
+
+        </p>
+        <ReactionButton post={post}/>
+    </article>
+    ))
   return (
-    <div>
-      
-    </div>
+    <section>
+        <h2>Posts</h2>
+        {renderedPosts}
+    </section>
   )
 }
 
